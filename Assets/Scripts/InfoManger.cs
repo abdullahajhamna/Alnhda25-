d1af2384c32f4932ce07bc99213df8ecd4764a25
abdullahajhamna <abdullahaalzlmt@gmail.com>
2024-09-04 11:13:46 +0300

    Alnhda25

---
+++ Assets/Scripts/InfoManger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class InfoManger : MonoBehaviour
+{
+  public void Info1()
+    {
+        SceneManager.LoadSceneAsync(7);
+    }
+  public void Info2()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+   public void Info3()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
---
+++ Assets/Scripts/WoodCollection2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WoodCollection2 : MonoBehaviour
+{
+    public int Wood = 0;
+    public TextMeshProUGUI woodtext;
+
+    // المرجع لسكربت CoinCollection
+    private CoinCollection coinCollection;
+
+    private void Start()
+    {
+        // العثور على سكربت CoinCollection الموجود على نفس GameObject أو أي كائن آخر
+        coinCollection = FindObjectOfType<CoinCollection>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Wood")
+        {
+            Wood++;
+            woodtext.text = Wood.ToString() + " /15";
+
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        
+        if ( coinCollection.Coin == 20 && Wood == 15)
+        {
+            SceneManager.LoadSceneAsync(6);
+        }
+    }
+}
